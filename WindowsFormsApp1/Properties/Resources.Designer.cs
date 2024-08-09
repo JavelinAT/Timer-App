d7a51b87e1c5229b89d0ee1ebaa301a86c5cd25c
JavelinAT <l200018@gmail.com>
2024-08-09 22:48:45 +0800

    建置

--- WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -79,5 +79,25 @@ namespace TimerApp.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查詢類似 (圖示) 的類型 System.Drawing.Icon 當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Icon LHU128 {
+            get {
+                object obj = ResourceManager.GetObject("LHU128", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查詢類似 (圖示) 的類型 System.Drawing.Icon 當地語系化資源。
+        /// </summary>
+        internal static System.Drawing.Icon LHU256 {
+            get {
+                object obj = ResourceManager.GetObject("LHU256", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
